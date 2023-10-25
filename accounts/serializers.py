a05b322f772987f2b4f8e462256869881b95428a
Krabs <vovastr0v@yandex.ru>
2023-10-25 20:12:23 +0500

    some permissions

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import User
 
 
@@ -8,4 +7,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        permission_classes = (IsAuthenticated,)
+        read_only_fields = ('is_superuser', 'is_staff', 'groups', 'user_permissions')
+        permission_classes = IsAuthenticated
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserSerializerAdmin(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        permission_classes = (IsAuthenticated, IsAdminUser)
+
+    def create(self, validated_data):
+        user = super(UserSerializerAdmin, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -3,16 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from .models import User
+from .permissions import IsOwnerOrReadOnly, IsStaffOrPostOnly
 from .serializers import *
+from rest_framework import permissions
 
 
 class UserAPICreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.user.is_active and self.request.user.is_superuser:
+            return UserSerializerAdmin
+        return UserSerializer
 
 
 class UserAPIDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly]
 
+    def get_serializer_class(self):
+        if self.request.user.is_active and self.request.user.is_superuser:
+            return UserSerializerAdmin
+        return UserSerializer
 
