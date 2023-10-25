8d88fb0bd588f8f5422d8bd3ab4b45eaebfdb651
Krabs <vovastr0v@yandex.ru>
2023-10-25 18:41:54 +0500

    jwt

--- BusinessWolf/urls.py
+++ BusinessWolf/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from course.urls import urlpatterns
 from accounts.views import *
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', UserAPICreateView.as_view()),
     path('api/users/<int:pk>/', UserAPIDetail.as_view()),
     path('api/drf-auth/', include('rest_framework.urls')),
-    path('api/auth/', include('djoser.urls')),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('', include(urlpatterns)),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),   # auth/token/login
 ]
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,21 +2,10 @@ from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 
 from .models import User
-from rest_framework.authtoken.models import Token
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'is_superuser']
+        fields = '__all__'
         permission_classes = (IsAuthenticated,)
-
-
-
-
-# class UserToken(serializers.ModelSerializer):
-#     user = Token.objects.get(key='token string').user
-#     foo = serializers.SerializerMethodField()
-#
-#     class Meta:
-#         model = Token
-#         fields = ['key', 'created', 'user_id']
