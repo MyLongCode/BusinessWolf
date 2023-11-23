4be2df3374dd3caa7b772ddcb14d8b026cd701bf
Krabs <vovastr0v@yandex.ru>
2023-11-23 15:46:34 +0500

    bfzfdfd

--- backend/course/views.py
+++ backend/course/views.py
@@ -117,7 +117,7 @@ class TestAPICreateView(generics.ListAPIView):
         if self.request.user.is_superuser:
             return Test.objects.all()
         user = self.request.user
-        return Test.objects.filter(module_id__in=Modules.objects.filter(course_id__in=Courses.objects.filter(
+        return Test.objects.filter(lesson_id__in=Modules.objects.filter(course_id__in=Courses.objects.filter(
             course_id__in=UserCourse.objects.filter(user_id=user.id).values_list("course_id", flat=True))).values_list(
             "module_id",
             flat=True))
